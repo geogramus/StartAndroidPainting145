723c88ecfb814724351f67007766cfbd7d0080b2
goshagrammatcikov <g.grammatchikov@localkitchen.ru>
2022-03-14 17:52:30 +0300

    github action test

--- app/src/main/java/com/example/startandroidpainting145/MainActivity.kt
+++ app/src/main/java/com/example/startandroidpainting145/MainActivity.kt
@@ -1,31 +1,27 @@
 package com.example.startandroidpainting145
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.PolyToPoly
-import kotlinx.android.synthetic.main.activity_main.PolyToPoly2
-import kotlinx.android.synthetic.main.activity_main.PolyToPoly3
-import kotlinx.android.synthetic.main.activity_main.PolyToPoly4
-import kotlinx.android.synthetic.main.activity_main.RectToRect
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RectToRect.setOnClickListener {
+        findViewById<View>(R.id.RectToRect).setOnClickListener {
             startActivity(Intent(this, RectToRectActivity::class.java))
         }
-        PolyToPoly.setOnClickListener {
+        findViewById<View>(R.id.PolyToPoly).setOnClickListener {
             startActivity(Intent(this, PolyToPolyActivity::class.java))
         }
-        PolyToPoly2.setOnClickListener {
+        findViewById<View>(R.id.PolyToPoly2).setOnClickListener {
             startActivity(Intent(this, PolyToPolyActivity2Dots::class.java))
         }
-        PolyToPoly3.setOnClickListener {
+        findViewById<View>(R.id.PolyToPoly3).setOnClickListener {
             startActivity(Intent(this, PolyToPolyActivity3Dots::class.java))
         }
-        PolyToPoly4.setOnClickListener {
+        findViewById<View>(R.id.PolyToPoly4).setOnClickListener {
             startActivity(Intent(this, PolyToPolyActivity4Dots::class.java))
         }
     }
